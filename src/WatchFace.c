5bf533046ebe0a0f4e8dfff181a227ea10f98dd2
danksalot <danksalot@users.noreply.github.com>
2016-07-18 23:27:38 -0500

    Changed to watch face

--- src/WatchFace.c
+++ src/WatchFace.c
@@ -1,45 +1,85 @@
 #include <pebble.h>
 
-static Window *s_window;
-static TextLayer *s_text_layer;
+static Window *s_main_window;
+static TextLayer *s_time_layer;
 
-static void init(void) {
-	// Create a window and get information about the window
-	s_window = window_create();
-  Layer *window_layer = window_get_root_layer(s_window);
-  GRect bounds = layer_get_bounds(window_layer);
-	
-  // Create a text layer and set the text
-	s_text_layer = text_layer_create(bounds);
-	text_layer_set_text(s_text_layer, "Hi, I'm a Pebble!");
-  
-  // Set the font and text alignment
-	text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-	text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+// Settings
+bool animations_flag;
+GColor background_color;
 
-	// Add the text layer to the window
-	layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_text_layer));
-  
-  // Enable text flow and paging on the text layer, with a slight inset of 10, for round screens
-  text_layer_enable_screen_text_flow_and_paging(s_text_layer, 10);
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
 
-	// Push the window, setting the window animation to 'true'
-	window_stack_push(s_window, true);
-	
-	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
 }
 
-static void deinit(void) {
-	// Destroy the text layer
-	text_layer_destroy(s_text_layer);
-	
-	// Destroy the window
-	window_destroy(s_window);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 102), bounds.size.w, 50));
+
+  // Improve the layout to be more like a watchface
+  window_set_background_color(s_main_window, background_color);
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+}
+
+static void init() {
+  // Set values for settings
+  animations_flag = true;
+  background_color = GColorVividCerulean;
+  
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with configured animations_flag
+  window_stack_push(s_main_window, animations_flag);
+
+  // Make sure the time is displayed from the start
+  update_time();
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
 }
 
 int main(void) {
-	init();
-	app_event_loop();
-	deinit();
+  init();
+  app_event_loop();
+  deinit();
 }
